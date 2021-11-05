a87855ab0aa700460230827c69d5fe4599deeb09
CongNam21 <CongNam21@users.noreply.github.com>
2021-11-05 22:12:52 +0700

    Create sanpham.py

---
+++ products__warranty_ver2/models/sanpham.py
@@ -0,0 +1,20 @@
+from odoo import api, fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    name = fields.Char(string='Tên sản phẩm', required=True)
+    date = fields.Datetime(string='Ngày nhận máy', required=True, default = lambda self: fields.Date.today())
+    hang = fields.Selection([
+          ('apple','Apple'),
+          ('ss', 'SamSung'),
+          ('vv', 'Vivo'),
+          ('sm', 'Xiaomi'),
+          ('Dell','Dell')
+     ], required=True, default='apple')
+    bh = fields.Boolean(string='Bảo hành', default=True)
+    seri = fields.Integer(string='Số seri', required=True)
+    nsx = fields.Many2one('hang.sp', string="Nhà sản xuất")
+
+
